718efd568526c977b2c9d57373ef35cc2cbc7e3f
smehta07 <33088560+smehta07@users.noreply.github.com>
2018-02-28 14:26:45 +0000

    Configggg

---
+++ eBaeFolder/config.php
@@ -0,0 +1,12 @@
+<?php
+// This script is used to establish connection with database
+
+// Define database connection parameters
+$host       = "gc06team37db.mysql.database.azure.com";
+$username   = "team37@gc06team37db";
+$password   = "Databases37!";
+$dbname     = "auction37gc06";
+
+// Establish connection
+$conn = mysqli_connect($host, $username, $password, $dbname) or die($conn->connect_error);
+
