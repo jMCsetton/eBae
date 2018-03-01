7e1a160e17faf281bb51815f81e07813721a280c
Sameen Asghar <Sameen@Sameens-MacBook-Pro.local>
2018-03-01 12:06:47 +0000

    commented form out

--- eBaeFolder/index.php
+++ eBaeFolder/index.php
@@ -62,14 +62,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
 
                  
-                                <form  action="index_php.php" method="post" autocomplete="off">
+                               <!-- <form  action="index_php.php" method="post" autocomplete="off"> -->
                                     <label style="color: #B33C12">Username</label>
                                     <input name="username" type="text" class="form-control" style="background-color: #e5e5e5" placeholder="Username">
 
                                     <label style="color: #B33C12">Password</label>
                                     <input name="password" type="password" class="form-control" style="background-color: #e5e5e5" placeholder="Password">
                                     <button class="btn btn-danger btn-block btn-round">Login</button>
-                                </form>
+                               
                                     <button class="btn btn-danger btn-block btn-round">Register New Account</button>
                                 
                                     </div>
