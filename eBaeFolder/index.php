eccd04fc93f7b6b6ffcb9244ac907f0980cbe11f
Shabri <home.shabri@gmail.com>
2018-03-01 19:21:04 +0000

    tried admin as array of sql query

--- eBaeFolder/index.php
+++ eBaeFolder/index.php
@@ -52,7 +52,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     //$myrole = $_POST['role'];
     
    
-    $sql = "SELECT username, password, role into a,b,c FROM user WHERE username = '$myusername' and password = '$mypassword'  ";
+    $sql = "SELECT username, password, role FROM user WHERE username = '$myusername' and password = '$mypassword'  and role = 'admim'";
     // $sql = "SELECT * FROM user WHERE username = '{$_POST['username']}' and password = '{$_POST['password']}'";
     $result = $conn->query($sql);
     //$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
@@ -62,8 +62,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     
     // If result matched $myusername and $mypassword, table row must be 1 row
       
-    if($count >= 1) {
-        if (c == "admin") {
+    //if($count >= 1) {
+        if ($result[2]== "admin") {
         
         //$row = $result->fetch_assoc()
         //$row=mysqli_fetch_assoc($result);
@@ -77,7 +77,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         }
         else{ header("Location: homepage.php");}
             //echo "ADMIN";
-        }
+       // }
         
        //session_register("myusername");
        //$_SESSION['login_user'] = $myusername;
@@ -88,15 +88,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
        
     
         
-    //$sql2 = "SELECT * FROM user WHERE username = '$myusername' and password = '$mypassword' and role = 'buyer_seller'  ";
-    //$result2 = $conn->query($sql2);
-    //$count2 = mysqli_num_rows($result2);
-    //if($count2 >= 1) {
+    $sql2 = "SELECT * FROM user WHERE username = '$myusername' and password = '$mypassword' and role = 'buyer_seller'  ";
+    $result2 = $conn->query($sql2);
+    $count2 = mysqli_num_rows($result2);
+    if($count2 >= 1) {
 
-      //  header("Location: homepage.php");
+        header("Location: homepage.php");
         
    
-      //}
+      }
       else {
         
         $error = "Your Login Name or Password is invalid";
