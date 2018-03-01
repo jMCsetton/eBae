d2d427c5e815110cccdd7e684d1a913477cf6ad8
Shabri <home.shabri@gmail.com>
2018-03-01 15:35:08 +0000

    back to *

--- eBaeFolder/index.php
+++ eBaeFolder/index.php
@@ -47,7 +47,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $myusername = mysqli_real_escape_string($db,$_POST['username']);
     $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
     
-    $sql = "SELECT userID FROM user WHERE username = '$myusername' AND password = '$mypassword'";
+    $sql = "SELECT * FROM user WHERE username = '$myusername' AND password = '$mypassword'";
     $result = mysqli_query($db,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
     $active = $row['active'];
