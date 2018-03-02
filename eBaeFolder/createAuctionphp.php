1f354548f0060820aa8763f4d8e90fe2e4a3e164
Shabri <home.shabri@gmail.com>
2018-03-02 20:57:02 +0000

    set again

--- eBaeFolder/createAuctionphp.php
+++ eBaeFolder/createAuctionphp.php
@@ -42,8 +42,8 @@ if (isset($_POST['submit']))
   $userID = $_SESSION['userID'];
   //$sql = "INSERT INTO product (productName, userID) VALUES ('".$_POST["productName"]."','$userID')";
   $sql = "INSERT INTO product (category, productName, productInfo, reservePrice, userID, quantity, conditions)
-  SET ('".$_POST["category"]."', '".$_POST["productName"]."', '".$_POST["productInfo"]."',
-  ".$_POST["reservePrice"].", '$userID', ".$_POST["quantity"].", '".$_POST["condition"]."')";
+  SET '".$_POST["category"]."', '".$_POST["productName"]."', '".$_POST["productInfo"]."',
+  ".$_POST["reservePrice"].", '$userID', ".$_POST["quantity"].", '".$_POST["condition"]."'";
 
   /*$sql = "INSERT INTO product (category, productName, productInfo, productImage, endDate, reservePrice, userID, quantity, condition')
   VALUES ('".$_POST["category"]."', '".$_POST["productName"]."', '".$_POST["productInfo"]."', '".$_POST["productImage"]."', '".$_POST["endDate"]."',
