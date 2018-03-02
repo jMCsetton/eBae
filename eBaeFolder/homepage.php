814b0cbb31123844163e7e4a82c4474b36ebb2df
Shabri <home.shabri@gmail.com>
2018-03-02 11:39:50 +0000

    icon logout

--- eBaeFolder/homepage.php
+++ eBaeFolder/homepage.php
@@ -35,8 +35,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
     <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-envelope fa-fw w3-margin-right"></i>MY BIDS</a>
     <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-envelope fa-fw w3-margin-right"></i>CREATE AUCTION</a>
     <a href="#contact" onclick="w3_close()" class="w3-bar-item w3-button w3-padding"><i class="fa fa-envelope fa-fw w3-margin-right"></i>WATCHLIST</a>
-    <a href="logout.php" onclick="w3_close()" class="btn btn-danger btn-lg"><i class="fa fa-envelope fa-fw w3-margin-right"></i>Log Out</a>
-
+    <a href="logout.php" onclick="w3_close()" class="w3-bar-item w3-button w3-padding" style="color: #ff0000"><i class="fa fa-close fa-fw w3-margin-right"></i>Log Out</a>
   </div>
   <div class="w3-panel w3-large">
     <i class="fa fa-facebook-official w3-hover-opacity"></i>
