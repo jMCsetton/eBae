37504a90909c45e75bac7b4b470ce6d592cb386f
Sameen Asghar <Sameen@pc-212-233.customer.sfields.nido.lan>
2018-03-02 18:00:50 +0000

    changed sql query+ added session statement in createAuctionphp.php

--- eBaeFolder/createAuctionphp.php
+++ eBaeFolder/createAuctionphp.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+$user = $_SESSION['userID'];
 if (isset($_POST['submit']))
 {
   require "config.php";
@@ -12,7 +14,9 @@ if (isset($_POST['submit']))
   /*$sql = "INSERT INTO product (category, productName, productInfo, productImage, endDate, reservePrice, userID, quantity, condition')
   VALUES ('".$_POST["category"]."', '".$_POST["productName"]."', '".$_POST["productInfo"]."', '".$_POST["productImage"]."', '".$_POST["endDate"]."',
   '".$_POST["reservePrice"]."', '".$_POST["$user"]."', '".$_POST["quantity"]."', '".$_POST["condition"]."')";*/
-  $sql = "INSERT INTO product (productName, userID) VALUES ('Jake', '4')";
+  
+  
+  $sql = "INSERT INTO product (productName, userID) VALUES ('".$_POST["productName"]."','".$_POST["$user"]."')";
 
   if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
