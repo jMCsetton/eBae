2816b118115133b7315c55a36713c4e5266cdf0c
Shabri <home.shabri@gmail.com>
2018-03-05 20:52:03 +0000

    error in ; ups

--- eBaeFolder/music.php
+++ eBaeFolder/music.php
@@ -96,7 +96,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
         // Fetching data from database
         //header("Content-type: image/png"); 
         if ($result == null) {
-            echo '<label>No auctions at this time</label> '
+            echo '<label>No auctions at this time</label> ';
         }
 		while ($row = mysqli_fetch_assoc($result)) {          
           //echo "<img src='picture/".$row2["productImage"]."' width='300' height='300'/>";
