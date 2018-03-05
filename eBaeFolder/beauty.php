215546fde301a170d8d49b64cbeeed17c051c5dd
Sameen Asghar <Sameen@pc-212-233.customer.sfields.nido.lan>
2018-03-05 22:05:10 +0000

    added if else in while

--- eBaeFolder/beauty.php
+++ eBaeFolder/beauty.php
@@ -112,7 +112,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
         
         // Fetching data from database
         //header("Content-type: image/png"); 
-				while ($row = mysqli_fetch_assoc($result)) {          
+				else { while ($row = mysqli_fetch_assoc($result)) {          
           //echo "<img src='picture/".$row2["productImage"]."' width='300' height='300'/>";
           //echo "<img src = '".base64_encode($row2["productImage"])."' width='300' height='300'/>";
           echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["productImage"] ).'" style="width:30%; height:30%" class="w3-third w3-container"/>';
@@ -132,7 +132,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
             </div>
             
               ';
-
+        }
               
 				}
 				?>
