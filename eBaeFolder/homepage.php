c67190da404a3e34977d17b8a8c447d8ea43e37b
Shabri <home.shabri@gmail.com>
2018-03-05 18:20:05 +0000

    added row to while

--- eBaeFolder/homepage.php
+++ eBaeFolder/homepage.php
@@ -91,7 +91,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
         ob_start();
         // Fetching data from database
         //header("Content-type: image/png"); 
-				while ($row2 = mysqli_fetch_assoc($result2)) {          
+				while ($row2 = mysqli_fetch_assoc($result2) && $row = mysqli_fetch_assoc($result)) {          
           //echo "<img src='picture/".$row2["productImage"]."' width='300' height='300'/>";
           //echo "<img src = '".base64_encode($row2["productImage"])."' width='300' height='300'/>";
           //echo '<img src="data:image/jpeg;base64,'.base64_encode( $row2["productImage"] ).'"/>';
