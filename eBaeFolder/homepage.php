e74e6bcec1904b5897efeaed2c4d2548c037d710
Shabri <home.shabri@gmail.com>
2018-03-05 18:48:18 +0000

    added breaks

--- eBaeFolder/homepage.php
+++ eBaeFolder/homepage.php
@@ -106,6 +106,8 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
               <br><label>Description: '.$row["productInfo"].'</label>
               <br>
               <br>
+              <br>
+              <br>
             </div>
             
               ';
