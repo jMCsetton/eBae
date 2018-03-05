70fa9787d4608c567b5295376c468a7d4ba3f9b6
Shabri <home.shabri@gmail.com>
2018-03-05 18:37:40 +0000

    styling

--- eBaeFolder/homepage.php
+++ eBaeFolder/homepage.php
@@ -94,9 +94,9 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 				while ($row = mysqli_fetch_assoc($result)) {          
           //echo "<img src='picture/".$row2["productImage"]."' width='300' height='300'/>";
           //echo "<img src = '".base64_encode($row2["productImage"])."' width='300' height='300'/>";
-          echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["productImage"] ).'" style="width:30%; height:30%" />';
+          echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["productImage"] ).'" style="width:30%; height:30%" class="w3-third w3-container"/>';
           echo '
-            <div >
+            <div class="w3-twothird w3-container">
               <h1>'.$row["productName"].'</h1>
               <label>Reserve Price:<p>'.$row["reservePrice"].'</p></label> 
               <label>End Date: <p>'.$row["endDate"].'</p></label>
