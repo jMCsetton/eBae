361df34577cdfce25b6afe94e47e264957b2f649
jMCsetton <jMCsetton@github.com>
2018-03-05 16:07:11 +0000

    is dis gd?

--- eBaeFolder/Registrationphp.php
+++ eBaeFolder/Registrationphp.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 ob_start();
 //$user = $_SESSION['userID'];
 if (isset($_POST['submit']))
@@ -11,20 +10,14 @@ if (isset($_POST['submit']))
   if ($conn->connect_error) {
     die("Connection failed: ".$conn->connect_error);
   }
-    
-  $userID = $_SESSION['userID'];
 
+  $sql = "INSERT INTO user (username, firstName, lastName, DOB, gender, email_ID, postCode, role, password)
+  VALUES ('".$_POST["username"]."', '".$_POST["firstName"]."', '".$_POST["lastName"]."', '".$_POST["DOB"]."',
+  '".$_POST["gender"]."', '".$_POST["email_ID"]."', '".$_POST["postCode"]."', '".$_POST["role"]."', '".$_POST["password"]."')";
 
-  $sql = "INSERT INTO user (userID, username, firstName, lastName, DOB, gender, email_ID, postCode, role, password)
-  VALUES ('".$_POST["userID"]."', '".$_POST["username"]."', '".$_POST["firstName"]."', 'lastName', '".$_POST["DOB"]."',
-  '".$_POST["gender"]."', '$userID', '".$_POST["quantity"]."', '".$_POST["condition"]."')";
-
-  /*$sql = "INSERT INTO product (category, productName, productInfo, productImage, endDate, reservePrice, userID, quantity, conditions)
-  VALUES ('".$_POST["category"]."', '".$_POST["productName"]."', '".$_POST["productInfo"]."', LOAD_FILE('".$_POST["productImage"]."'), '".$_POST["endDate"]."',
-  '".$_POST["reservePrice"]."', '$userID', '".$_POST["quantity"]."', '".$_POST["condition"]."')";*/
 
   if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+    echo "New user created created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
