fb235c65605057ba231fea61dd95a73a7c3ba2a1
Sameen Asghar <Sameen@pc-212-233.customer.sfields.nido.lan>
2018-03-05 20:45:30 +0000

    changed if statement

--- eBaeFolder/beauty.php
+++ eBaeFolder/beauty.php
@@ -119,7 +119,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 
               
 
-              if (!$row = mysqli_fetch_assoc($result)){
+              if ($row = mysqli_fetch_assoc($result) == null){
                 echo '<label> No items to show</label> ';
 
 
