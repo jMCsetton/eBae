4cf9d47c99a536904c9b645be1a87c8a042822e6
Shabri <home.shabri@gmail.com>
2018-03-07 20:38:26 +0000

    put address sql back in

--- eBaeFolder/Registrationphp.php
+++ eBaeFolder/Registrationphp.php
@@ -14,19 +14,19 @@ if (isset($_POST['submit']))
 
 
 
-  /*$sql = "INSERT INTO address (postCode, street, city, county, doorNumber, username) VALUES ('E1 7JS',
-  '".$POST["street"]."', '".$POST["city"]."', '".$POST["country"]."', '".$POST["doorNumber"]."', '".$POST["username"]."')";*/
+  $sql = "INSERT INTO address (postCode, street, city, county, doorNumber, username) VALUES ('".$_POST["postCode"]."',
+  '".$POST["street"]."', '".$POST["city"]."', '".$POST["country"]."', '".$POST["doorNumber"]."', '".$POST["username"]."')";
 
   $sql2 = "INSERT INTO user (username, firstName, lastName, DOB, gender, email_ID, postCode, role, password)
   VALUES ('".$_POST["username"]."', '".$_POST["firstName"]."', '".$_POST["lastName"]."', '".$_POST["DOB"]."',
-  '".$_POST["gender"]."', '".$_POST["email_ID"]."', 'E1 7HS', 'buyer_seller', '".$_POST["password"]."')";
+  '".$_POST["gender"]."', '".$_POST["email_ID"]."', '".$_POST["postCode"]."', 'buyer_seller', '".$_POST["password"]."')";
 
   //".$_POST["postCode"]."
-  /*if ($conn->query($sql) === TRUE ) {
+if ($conn->query($sql) === TRUE ) {
     echo "New user address created created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
-}*/
+}
 if ($conn->query($sql2) === TRUE ) {
   echo "New user table row created created successfully";
 } else {
