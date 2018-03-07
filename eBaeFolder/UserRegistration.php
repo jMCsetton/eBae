fa1883c6f227956a3514f3b20b76994aa2daa539
Sameen Asghar <Sameen@eduroam-int-dhcp-97-242-115.ucl.ac.uk>
2018-03-07 18:21:23 +0000

    removed php from userregistration

--- eBaeFolder/UserRegistration.php
+++ eBaeFolder/UserRegistration.php
@@ -102,47 +102,7 @@ ob_start();
     </div>
 </body>
 
-<?php
 
-if (isset($_POST['submit']))
-{
-  require "config.php";
-  $conn =  new mysqli($host, $username, $password, $dbname);
-
-  // Check connection
-  if ($conn->connect_error) {
-    die("Connection failed: ".$conn->connect_error);
-  }
-
- $sql = "INSERT INTO address (postCode, street, city, country, doorNumber, username) VALUES ('".$POST["postCode"]."',
-  '".$POST["street"]."', '".$POST["city"]."', '".$POST["country"]."', '".$POST["doorNumber"]."', '".$POST["username"]."')";
-
-  $sql2 = "INSERT INTO user (username, firstName, lastName, DOB, gender, email_ID, postCode, role, password)
-  VALUES ('".$_POST["username"]."', '".$_POST["firstName"]."', '".$_POST["lastName"]."', '".$_POST["DOB"]."',
-  '".$_POST["gender"]."', '".$_POST["email_ID"]."', '".$_POST["postCode"]."', '".$_POST["role"]."', '".$_POST["password"]."')";
-
-
-  //$sql = "INSERT INTO address (postCode) VALUES ('".$POST["postCode"]."')";
-  
-  if ($conn->query($sql) === TRUE) {
-    echo "New user created created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-if ($conn->query($sql2) === TRUE) {
-    echo "New user created created successfully";
-} else {
-    echo "Error: " . $sql2 . "<br>" . $conn->error;
-}
-
-
-  //}
-
-}
-
-
- ?>
 <!-- Core JS Files -->
 <script src="../assets/js/jquery-3.2.1.js" type="text/javascript"></script>
 <script src="../assets/js/jquery-ui-1.12.1.custom.min.js" type="text/javascript"></script>
