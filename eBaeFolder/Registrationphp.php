068ee7bbf6938ff62ed5fcb1d3ce3db23f8d888c
Shabri <home.shabri@gmail.com>
2018-03-07 20:11:04 +0000

    uncommented sql2 query

--- eBaeFolder/Registrationphp.php
+++ eBaeFolder/Registrationphp.php
@@ -17,15 +17,20 @@ if (isset($_POST['submit']))
   $sql = "INSERT INTO address (postCode, street, city, county, doorNumber, username) VALUES ('".$POST["postCode"]."',
   '".$POST["street"]."', '".$POST["city"]."', '".$POST["country"]."', '".$POST["doorNumber"]."', '".$POST["username"]."')";
 
-  /*$sql = "INSERT INTO user (username, firstName, lastName, DOB, gender, email_ID, postCode, role, password)
+  $sql2 = "INSERT INTO user (username, firstName, lastName, DOB, gender, email_ID, postCode, role, password)
   VALUES ('".$_POST["username"]."', '".$_POST["firstName"]."', '".$_POST["lastName"]."', '".$_POST["DOB"]."',
-  '".$_POST["gender"]."', '".$_POST["email_ID"]."', '".$_POST["postCode"]."', '".$_POST["role"]."', '".$_POST["password"]."')";*/
+  '".$_POST["gender"]."', '".$_POST["email_ID"]."', '".$_POST["postCode"]."', '".$_POST["role"]."', '".$_POST["password"]."')";
 
 
-  if ($conn->query($sql) === TRUE) {
-    echo "New user created created successfully";
+  if ($conn->query($sql) === TRUE ) {
+    echo "New user address created created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
+}
+if ($conn->query($sql2) === TRUE ) {
+  echo "New user table row created created successfully";
+} else {
+  echo "Error: " . $sql2 . "<br>" . $conn->error;
 }
 
   //}
