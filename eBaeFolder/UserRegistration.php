be756f2b9e5838b1721e4dabcd059371cf005a06
Amelia <ameliaoduffy@eduroam-int-dhcp-97-158-245.ucl.ac.uk>
2018-03-07 16:16:21 +0000

    shtuff

--- eBaeFolder/UserRegistration.php
+++ eBaeFolder/UserRegistration.php
@@ -85,7 +85,7 @@ ob_start();
                                     <input type="password" class="form-control" placeholder="Confirm Password">
                                      
                                    
-                                    <button type="submit" name="submit" class="btn btn-danger btn-block btn-round"><a href="https://gc06team37db.azurewebsites.net">Register</a></button>
+                                    <button type="submit" name="submit" class="btn btn-danger btn-block btn-round">Register</button>
 
                                   
                                 </form>
