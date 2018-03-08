e6d9152b81fa1b6a56d151373c852f7ebbc56076
Shabri <home.shabri@gmail.com>
2018-03-08 15:48:49 +0000

    table width

--- eBaeFolder/auctionDetails.php
+++ eBaeFolder/auctionDetails.php
@@ -146,7 +146,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 				
 				?>
 
-<div class="table-responsive" style="width: 50%">
+<div class="table-responsive" style="width: 80%">
 			<table id="bid_data" class="table table-striped table-bordered">
 				<thead>
 					<tr>
