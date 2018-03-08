411883177d78f57526ca1a1bc2df6773dc401645
Shabri <home.shabri@gmail.com>
2018-03-08 17:42:36 +0000

    changes to webjob

---
+++ eBaeFolder/webJob.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'config.php';
+$conn =  new mysqli($host, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: ".$conn->connect_error);
+  }
+
+$date = date("Y/m/d");
+$sql = "INSERT INTO system (date) VALUES ('$date')";
+
+?>
