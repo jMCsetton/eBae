e28b1891959a51bad51d225e86c6a9af8a0d15a8
Shabri <home.shabri@gmail.com>
2018-03-08 20:11:00 +0000

    changes

--- eBaeFolder/webJob.php
+++ eBaeFolder/webJob.php
@@ -1,16 +1,13 @@
 <?php
 
-
-$host       = "gc06team37db.mysql.database.azure.com";
-$username   = "team37@gc06team37db";
-$password   = "Databases37!";
-$dbname     = "auction37gc06";
-$conn =  new mysqli($host, $username, $password, $dbname);
+  require "config.php";
+  $conn =  new mysqli($host, $username, $password, $dbname);
 
   // Check connection
   if ($conn->connect_error) {
     die("Connection failed: ".$conn->connect_error);
   }
+  
 
 $date = date("Y/m/d");
 $sql = "INSERT INTO system (date) VALUES ('$date')";
