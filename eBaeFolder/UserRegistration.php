ee9b39bab8511378895c31bc32ee33d4ffecf0e5
Amelia <ameliaoduffy@eduroam-int-dhcp-97-252-5.ucl.ac.uk>
2018-03-09 12:10:35 +0000

    password

--- eBaeFolder/UserRegistration.php
+++ eBaeFolder/UserRegistration.php
@@ -78,17 +78,13 @@
 
 
                                    <!--  <input type="password" class="form-control" placeholder="Password" name="password"/> -->
+
+                                     <label>Password/label>
                                     <input type="password" class="form-control" placeholder="Password" id="psw" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}">
 
                                      <label>Confirm Password</label>
                                     <input type="password" class="form-control" placeholder="Confirm Password" id="psw2"/>
                                     <span id='message'></span>
-
-                                    <input type="password" class="form-control" placeholder="Password" name="password">
-
-                                     <label>Confirm Password</label>
-                                    <input type="password" class="form-control" placeholder="Confirm Password">
-
                                      
                                    
                                     <button type="submit" name="submit" class="btn btn-danger btn-block btn-round"><a href="https://gc06team37db.azurewebsites.net">Register</a></button>
