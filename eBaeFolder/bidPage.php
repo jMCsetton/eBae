7485d7e55c6efefdc21887b3a4d46cb2d93e4367
Shabri <home.shabri@gmail.com>
2018-03-09 12:58:54 +0000

    added max price

--- eBaeFolder/bidPage.php
+++ eBaeFolder/bidPage.php
@@ -140,7 +140,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
              <td>'.$row["productName"].'</td>
              <td>'.$row["bidDate"].'</td>
              <td>'.$row["reservePrice"].'</td>
-             <td>'.$row["bidPrice"].'</td> 
+             <td>'.$row["maxprice"].'</td> 
              
 
     
