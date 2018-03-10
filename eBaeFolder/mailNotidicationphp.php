df585bade22efc4c1abf5df5797273da20608fad
Shabri <home.shabri@gmail.com>
2018-03-10 21:11:59 +0000

    noti push

--- eBaeFolder/mailNotidicationphp.php
+++ eBaeFolder/mailNotidicationphp.php
@@ -21,7 +21,7 @@ AND p.productID = a.productID";
 $result = $conn->query($sql);
 
 // gets seller emails
-$sql2 = "SELECT u.email_ID, a.productID, p.userID, a.auctionDate, p.productName
+$sql2 = "SELECT u.email_ID, a.productID, p.userID, a.auctionDate, p.productName, a.auctionPrice
 FROM user u, auction a, product p
 WHERE a.auctionDate = curdate()-1
 AND u.userID = p.userID
@@ -79,5 +79,39 @@ curl_close($session);
 // print everything out
 print_r($response);
 }
+
+while( $row2 = mysqli_fetch_array($result2)) { 
+  $params = array(
+       'api_user' => $user,
+       'api_key' => $pass,
+       'to' => $row2['email_ID'],
+       'subject' => 'Confirming your bought item!',
+       'html' => 'Hi! Thank you for your selling of product: '.$row2['productName'].' for '.$row2['auctionPrice'].'',
+       'text' => 'Hi! Thank you for your selling of product: '.$row2['productName'].' for '.$row2['auctionPrice'].'',
+       'from' => 'noreply@eBae.com',
+    );
+  
+  $request = $url.'api/mail.send.json';
+  
+  // Generate curl request
+  $session = curl_init($request);
+  
+  // Tell curl to use HTTP POST
+  curl_setopt ($session, CURLOPT_POST, true);
+  
+  // Tell curl that this is the body of the POST
+  curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+  
+  // Tell curl not to return headers, but do return the response
+  curl_setopt($session, CURLOPT_HEADER, false);
+  curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+  
+  // obtain response
+  $response = curl_exec($session);
+  curl_close($session);
+  
+  // print everything out
+  print_r($response);
+  }
 
 ?>
