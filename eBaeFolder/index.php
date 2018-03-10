f92a14cf9f084cc0fbd1db94acbdf570411b96ec
Sameen Asghar <Sameen@pc-212-233.customer.sfields.nido.lan>
2018-03-10 18:17:42 +0000

    moved mail stuff

--- eBaeFolder/index.php
+++ eBaeFolder/index.php
@@ -36,6 +36,43 @@ session_start();
 
 if (isset($_POST['submit'])) {
 
+    $url = 'https://api.sendgrid.com/';
+    $user = 'azure_0bfcaee4a89f20464352ea9333a7cfae@azure.com';
+    $pass = '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';
+   
+    $params = array(
+         'api_user' => $user,
+         'api_key' => $pass,
+         'to' => 'home.shabri@gmail.com',
+         'subject' => 'testing from curl',
+         'html' => 'testing body',
+         'text' => 'testing body',
+         'from' => 'zceesas@ucl.ac.uk',
+      );
+   
+    $request = $url.'api/mail.send.json';
+   
+    // Generate curl request
+    $session = curl_init($request);
+   
+    // Tell curl to use HTTP POST
+    curl_setopt ($session, CURLOPT_POST, true);
+   
+    // Tell curl that this is the body of the POST
+    curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
+   
+    // Tell curl not to return headers, but do return the response
+    curl_setopt($session, CURLOPT_HEADER, false);
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+   
+    // obtain response
+    $response = curl_exec($session);
+    curl_close($session);
+   
+    // print everything out
+    print_r($response);
+
+
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     // username and password sent from form 
     
@@ -92,42 +129,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
      
 
 
-
- $url = 'https://api.sendgrid.com/';
- $user = 'azure_0bfcaee4a89f20464352ea9333a7cfae@azure.com';
- $pass = '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';
-
- $params = array(
-      'api_user' => $user,
-      'api_key' => $pass,
-      'to' => 'home.shabri@gmail.com',
-      'subject' => 'testing from curl',
-      'html' => 'testing body',
-      'text' => 'testing body',
-      'from' => 'zceesas@ucl.ac.uk',
-   );
-
- $request = $url.'api/mail.send.json';
-
- // Generate curl request
- $session = curl_init($request);
-
- // Tell curl to use HTTP POST
- curl_setopt ($session, CURLOPT_POST, true);
-
- // Tell curl that this is the body of the POST
- curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-
- // Tell curl not to return headers, but do return the response
- curl_setopt($session, CURLOPT_HEADER, false);
- curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-
- // obtain response
- $response = curl_exec($session);
- curl_close($session);
-
- // print everything out
- print_r($response);
     
 }
     
