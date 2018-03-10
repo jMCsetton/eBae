56fcf27a583d12a8d8f46146dda58a1fe8630604
Sameen Asghar <Sameen@pc-212-233.customer.sfields.nido.lan>
2018-03-10 18:29:06 +0000

    removed mail php from index

--- eBaeFolder/index.php
+++ eBaeFolder/index.php
@@ -36,42 +36,6 @@ session_start();
 
 if (isset($_POST['submit'])) {
 
-    $url = 'https://api.sendgrid.com/';
-    $user = 'azure_0bfcaee4a89f20464352ea9333a7cfae@azure.com';
-    $pass = 'Databases37!';
-   
-    $params = array(
-         'api_user' => $user,
-         'api_key' => $pass,
-         'to' => 'home.shabri@gmail.com',
-         'subject' => 'testing from curl',
-         'html' => 'testing body',
-         'text' => 'testing body',
-         'from' => 'noreply@eBae.com',
-      );
-   
-    $request = $url.'api/mail.send.json';
-   
-    // Generate curl request
-    $session = curl_init($request);
-   
-    // Tell curl to use HTTP POST
-    curl_setopt ($session, CURLOPT_POST, true);
-   
-    // Tell curl that this is the body of the POST
-    curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-   
-    // Tell curl not to return headers, but do return the response
-    curl_setopt($session, CURLOPT_HEADER, false);
-    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-   
-    // obtain response
-    $response = curl_exec($session);
-    curl_close($session);
-   
-    // print everything out
-    print_r($response);
-
 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     // username and password sent from form 
