58660736d8d28ac23c4fcf7d11fbaf2a5ca27337
jMCsetton <jMCsetton@github.com>
2018-03-11 13:19:47 +0000

    i don't know how i got it working :(

--- eBaeFolder/Registrationphp.php
+++ eBaeFolder/Registrationphp.php
@@ -11,14 +11,16 @@ if (isset($_POST['submit']))
   if ($conn->connect_error) {
     die("Connection failed: ".$conn->connect_error);
   }
+  
+
 
   $sql = "INSERT INTO address (postCode, street, city, county, doorNumber, username) VALUES ('".$_POST["postCode"]."','".$_POST["street"]."', '".$_POST["city"]."', '".$_POST["county"]."', '".$_POST["doorNumber"]."', '".$_POST["username"]."')";
   
   $sql2 = "INSERT INTO user (username, firstName, lastName, DOB, gender, email_ID, postCode, role, password)
   VALUES ('".$_POST["username"]."', '".$_POST["firstName"]."', '".$_POST["lastName"]."', '".$_POST["DOB"]."',
   '".$_POST["gender"]."', '".$_POST["email_ID"]."', '".$_POST["postCode"]."', 'buyer_seller', '".$_POST["password"]."')";
-  mysqli_query($conn, $sql);
-  mysqli_query($conn, $sql);
+  mysql_query($sql, $conn);
+  mysql_query($sql2, $conn);
 
   if ($conn->query($sql) === TRUE) {
     echo "New user created created successfully";
