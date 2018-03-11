861402b18680c196b96c456fcb93c348fbc01f87
jMCsetton <jMCsetton@github.com>
2018-03-11 18:23:08 +0000

    fuck this

--- eBaeFolder/Registrationphp.php
+++ eBaeFolder/Registrationphp.php
@@ -11,10 +11,7 @@ if (isset($_POST['submit']))
   if ($conn->connect_error) {
     die("Connection failed: ".$conn->connect_error);
   }
-  $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-  if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    echo "$email is a valid email address.";
-  } 
+
   $sql = "INSERT INTO address (postCode, street, city, county, doorNumber, username) VALUES ('".$_POST["postCode"]."','".$_POST["street"]."', '".$_POST["city"]."', '".$_POST["county"]."', '".$_POST["doorNumber"]."', '".$_POST["username"]."')";
   mysqli_query($sql, $conn);
   $sql2 = "INSERT INTO user (username, firstName, lastName, DOB, gender, email_ID, postCode, role, password)
