799b8ba84fb15940d88769e1fae5d1b022c34c0d
Shabri <home.shabri@gmail.com>
2018-03-11 18:33:31 +0000

    added seller feedback

---
+++ eBaeFolder/giveSellerFeedback.php
@@ -0,0 +1,14 @@
+<?php
+
+$error = "<label>Rating</label>
+<select name='Rating'>
+  <option value='1 Star'>1 Star</option>
+  <option value='2 Star'>2 Stars</option>
+  <option value='3 Star'>3 Stars</option>
+  <option value='4 Star'>4 Stars</option>
+  <option value='5 Star'>5 Stars</option>
+</select>";
+
+echo "<script type='text/javascript'>alert('$error');</script>";
+?>
+
