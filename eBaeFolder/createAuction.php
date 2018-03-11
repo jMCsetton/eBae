a7b356fe4c222e3ce08479cba4e6e18fad0483f8
Amelia <ameliaoduffy@Amelias-MacBook-Pro.local>
2018-03-11 20:08:01 +0000

    wooo it works

--- eBaeFolder/createAuction.php
+++ eBaeFolder/createAuction.php
@@ -177,25 +177,11 @@ function validateForm() {
       return false;
     }
 
-    // var today = new Date();
-    // var dd = today.getDate();
-    // var mm = today.getMonth()+1; //January is 0!
-    // var yyyy = today.getFullYear();
-    
-    // if(dd<10){
-    //     dd='0'+dd;
-    // } 
-    // if(mm<10){
-    //     mm='0'+mm;
-    // }
-
-    // var today = yyyy+'/'+mm+'/'+dd; 
-
     var inputtedDate = document.forms["auctionForm"]["endDate"].value = new Date();
     var today = new Date();
     
     if (inputtedDate.getTime() < today.getTime()) {
-      alert("The first date is after the second date!");
+      alert("Please choose a valid date");
       return false;
   }  
 }
