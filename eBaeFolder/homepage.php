bdf73cc5759f22adedb49f00b7058db204e84844
jMCsetton <jMCsetton@github.com>
2018-03-12 19:17:18 +0000

    ffs

--- eBaeFolder/homepage.php
+++ eBaeFolder/homepage.php
@@ -161,7 +161,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
   <div> 
   </div>
 
- <div class="w3-container"; style="width:1200px; height:400px; overflow: scroll; overflow-y: hidden">
+ <div class="w3-container"; style="width:100%; height:400px; overflow: scroll; overflow-y: scroll">
  <?php
         ob_start();
 
