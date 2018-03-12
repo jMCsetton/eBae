eaccd7d2c6886000c63d5a79990a9bb4db55c256
jMCsetton <jMCsetton@github.com>
2018-03-12 19:07:49 +0000

    oops lel

--- eBaeFolder/homepage.php
+++ eBaeFolder/homepage.php
@@ -161,7 +161,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
   <div> 
   </div>
 
- <div style="overflow-y: scroll">
+ <div style="width:100%; overflow-y: scroll">
  <?php
         ob_start();
 
