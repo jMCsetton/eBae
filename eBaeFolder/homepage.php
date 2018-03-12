745b5da103a6040ba990461d6a707f784a04e77d
Amelia <ameliaoduffy@Amelias-MacBook-Pro.local>
2018-03-12 20:37:05 +0000

    this doesnt need to scroll

--- eBaeFolder/homepage.php
+++ eBaeFolder/homepage.php
@@ -161,11 +161,10 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
   <div> 
   </div>
   
-
- <div style="overflow-x: scroll; overflow: auto; overflow-y: hidden; white-space: nowrap;">
- <div class="topPicks" style="font-size: 25px; font-weight: bold; padding-left: 30px;">
+<div class="topPicks" style="font-size: 25px; font-weight: bold; padding-left: 30px;">
  <h1>Our current top picks:</h1>
  </div>
+ <div style="overflow-x: scroll; overflow: auto; overflow-y: hidden; white-space: nowrap;">
  <?php
         ob_start();
 
