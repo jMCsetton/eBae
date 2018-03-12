55da0672693bd0b70af5f4f2aabec2bf2890aedc
jMCsetton <jMCsetton@github.com>
2018-03-12 19:02:30 +0000

    broke it more :(

--- eBaeFolder/homepage.php
+++ eBaeFolder/homepage.php
@@ -161,24 +161,24 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
   <div> 
   </div>
 
- <div>
+  
  <?php
         ob_start();
 
         while ($row = mysqli_fetch_assoc($resultJA)) {
         ?>
-         <div class="w3-container" style="width: 16%; display: inline-block; background-color:blue; white-space: nowrap">
+         <div class="w3-container" style="display: inline-block; background-color:blue; white-space: nowrap">
            <figure>
            <div class="image" style="display: inline; float:left;">
              <?php
-           echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["productImage"] ).'" class="w3-container"/>';
+           echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["productImage"] ).'" style=" width:150px; height:22%; vertical-align: top; class="w3-container"/>';
            ?>
            </div>
-           <figcaption style="font-weight:bold; width:90%; word-wrap:break-word;">
+           <figcaption style="font-weight:bold; width:100px; word-wrap:break-word;">
              <?php
           $_SESSION['productID'] = $row['productID'];
           $productID = $_SESSION['productID'];
-          echo "<a href='auctionDetails.php?id=".$row['productID']."'><b>".$row["productName"]."</b> </a> 
+          echo "<a href='auctionDetails.php?id=".$row['productID']."' class=' w3-container'><b>".$row["productName"]."</b> </a> 
           ";
           echo '<div>
               <label>Viewing Traffic: '.$row["trafficFrequencyPerItem"].'</label>         
@@ -196,7 +196,6 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
 <?php
 }
 ?>
-</div>
       
        
 
