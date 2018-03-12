f7ed8fd0986d2eab254b7e723540741ed7c6d12e
Shabri <home.shabri@gmail.com>
2018-03-12 18:02:23 +0000

    trying

--- eBaeFolder/myAuctionsPage.php
+++ eBaeFolder/myAuctionsPage.php
@@ -224,16 +224,16 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
                        <td>'.$row2["buyername"].'</td>
                      
                        ';
-                       if ($row2["buyername"] == 'sameen') {
-                        echo "<td><a href='giveSellerFeedback.php?id=".$row2['productid']."' class='w3-third w3-container' style='background-color:black; width:50%; color:white'><b>Rate User<b></a> 
-                        </td> </tr>";
+                       if ($row2["buyername"] == 'Auction Still Open') {
+                        echo '<td>Not Applicable</td> </tr>';
           
-                       } else if ($row2["buyername"] == 'sameen') {
-                          echo "<td><a href='giveSellerFeedback.php?id=".$row2['productid']."' class='w3-third w3-container' style='background-color:black; width:50%; color:white'><b>Rate User<b></a> 
-                        </td> </tr>";
+                       } else if ($row2["buyername"] == 'Not sold') {
+                        echo '<td>Not Applicable</td> </tr>';
                        } 
                        else {
-                         echo '<td>Not Applicable</td> </tr>';
+                         
+                         echo "<td><a href='giveSellerFeedback.php?id=".$row2['productid']."' class='w3-third w3-container' style='background-color:black; width:50%; color:white'><b>Rate User<b></a> 
+                        </td> </tr>";
                        }
            
            
