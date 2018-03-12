52de78bc18ca9cb356ba10ed2c94dde533e58468
Amelia <ameliaoduffy@Amelias-MacBook-Pro.local>
2018-03-12 16:39:56 +0000

    ghmmmmm

--- eBaeFolder/homepage.php
+++ eBaeFolder/homepage.php
@@ -170,7 +170,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
            echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["productImage"] ).'" style="width:22%; height:22% inline-block" class="w3-third w3-container"/>';
           $_SESSION['productID'] = $row['productID'];
           $productID = $_SESSION['productID'];
-          echo "<a href='auctionDetails.php?id=".$row['productID']."' class=' w3-container' style='background-color:black; width:9%; color:white'><h1>'.$row["productName"].'</h1> </a> 
+          echo "<a href='auctionDetails.php?id=".$row['productID']."' class=' w3-container' style='background-color:black; width:9%; color:white'><b>".$row["productName"]."</b> </a> 
           ";
           echo '
             <div style= "bg-colour:white" class="inline-block w3-container">
