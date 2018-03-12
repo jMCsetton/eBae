ee45cc0fe256ff779bd2cca506af76ce3989ef33
Shabri <home.shabri@gmail.com>
2018-03-12 18:00:49 +0000

    testing

--- eBaeFolder/myAuctionsPage.php
+++ eBaeFolder/myAuctionsPage.php
@@ -224,7 +224,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
                        <td>'.$row2["buyername"].'</td>
                      
                        ';
-                       if ($row2["buyername"] == 'Auction Still Open') {
+                       if ($row2["buyername"] == 'sameen') {
                         echo "<td><a href='giveSellerFeedback.php?id=".$row2['productid']."' class='w3-third w3-container' style='background-color:black; width:50%; color:white'><b>Rate User<b></a> 
                         </td> </tr>";
           
