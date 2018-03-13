8a54432f474c049b5b5df7f9483931bfdcf09da3
Amelia <ameliaoduffy@Amelias-MacBook-Pro.local>
2018-03-13 13:59:50 +0000

    hmmm

--- eBaeFolder/adminRegistrationphp.php
+++ eBaeFolder/adminRegistrationphp.php
@@ -13,9 +13,9 @@ if (isset($_POST['submit']))
   }
   
   
-  $sql2 = "INSERT INTO user (username, firstName, lastName, DOB, gender, email_ID, postCode, role, password)
+  $sql2 = "INSERT INTO user (username, firstName, lastName, DOB, gender, email_ID, role, password)
   VALUES ('".$_POST["username"]."', '".$_POST["firstName"]."', '".$_POST["lastName"]."', '".$_POST["DOB"]."',
-  '".$_POST["gender"]."', '".$_POST["email_ID"]."', 'null', 'admin', '".$_POST["password"]."')";
+  '".$_POST["gender"]."', '".$_POST["email_ID"]."', 'admin', '".$_POST["password"]."')";
   mysqli_query($sql2, $conn);
 
   if ($conn->query($sql2) === TRUE) {
