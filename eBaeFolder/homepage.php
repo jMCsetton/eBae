c67a5c2ab9cccc9dc5822b8a21bac561664fe3bf
jMCsetton <jMCsetton@github.com>
2018-03-13 18:55:47 +0000

    dunno anymore

--- eBaeFolder/homepage.php
+++ eBaeFolder/homepage.php
@@ -250,7 +250,7 @@ body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", sans-serif}
            echo '<img src="data:image/jpeg;base64,'.base64_encode( $row["productImage"] ).'" style=" width:150px; height:22%; vertical-align: top; class="w3-container"/>';
            ?>
            </div>
-           <figcaption style="font-weight:bold; width:100px; word-wrap:break-word; text-align: center;">
+           <figcaption id="capt" style="font-weight:bold; width:100px; word-wrap:break-word; text-align: center;">
              <?php
           $_SESSION['productID'] = $row['productID'];
           $productID = $_SESSION['productID'];
@@ -335,7 +335,7 @@ function w3_close() {
 }
 window.onload = hiderec();
 function hiderec() {
-      if (document.getElementById("recitems").innerHTML == "") {
+      if (document.getElementById("capt").innerHTML == "") {
         alert("i did it!!!");
         document.getElementById("msg").innerHTML = "You don't have any recommended items yet... Get bidding!";
       }
