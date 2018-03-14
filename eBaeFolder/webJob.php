26bde9cfc7f8b17bc7e7c8def6e0e64d11148892
Shabri <home.shabri@gmail.com>
2018-03-14 21:32:12 +0000

    webjob

--- eBaeFolder/webJob.php
+++ eBaeFolder/webJob.php
@@ -30,7 +30,7 @@ if ($conn->query($sql) === TRUE) {
     WHERE endDate = CURDATE()";
     $result2 = $conn->query($sql2);*/
 
-    $sql3 = "SELECT p.endDate, p.productID, b.userID, b.bidID, bidPriceHighest as bidPrice , p.reservePrice
+    /*$sql3 = "SELECT p.endDate, p.productID, b.userID, b.bidID, bidPriceHighest as bidPrice , p.reservePrice
     FROM (SELECT productID, MAX(bidPrice) AS bidPriceHighest, date_format(bidDate, '%d-%m-%Y') bidDate
    FROM bid
    GROUP BY productID
@@ -38,7 +38,19 @@ if ($conn->query($sql) === TRUE) {
     WHERE p.productID = b.productID
     AND r.productID = b.productID
     AND endDate = CURDATE()-1
-    GROUP BY productID;"; 
+    GROUP BY productID;"; */
+
+    $sql3="SELECT p.endDate, p.productID, p.userID, b.bidID, bidPriceHighest as bidPrice , p.reservePrice, u.email_ID, p.productName
+    from product p
+   LEFT OUTER JOIN bid b ON p.productID = b.productID
+  LEFT OUTER JOIN (SELECT productID, MAX(bidPrice) AS bidPriceHighest, date_format(bidDate, '%d-%m-%Y') bidDate
+   FROM bid
+   GROUP BY productID
+  ) r ON r.productID = p.productID
+  LEFT JOIN user u ON p.userID = u.userID
+  WHERE endDate = '2018-03-23'
+GROUP BY p.productID";
+
     $result3 = $conn->query($sql3);
 
     if ($conn->query($sql3) === TRUE) {
@@ -46,6 +58,32 @@ if ($conn->query($sql) === TRUE) {
       } else {
         echo "Error: " . $sql3 . "<br>" . $conn->error;
       }
+    
+      use PHPMailer\PHPMailer\PHPMailer;
+      use PHPMailer\PHPMailer\Exception;
+      
+      require_once('./vendor/autoload.php');
+      
+      $mail = new phpmailer(true);
+      
+      
+      //Server settings
+      $mail->isSMTP();
+      $mail->SMTPDebug = 2;
+      $mail->Host = 'smtp.gmail.com';
+      $mail->Port = 587;
+      $mail->SMTPSecure = 'tls'; // enable 'tls'  to prevent security issues
+      $mail->SMTPAuth = true;
+      $mail->Username = 'ebaeauction@gmail.com';
+      $mail->Password = 'Databases37!';
+      // walkaround to bypass server errors
+      $mail->SMTPOptions = array(
+      'ssl' => array(
+          'verify_peer' => false,
+          'verify_peer_name' => false,
+          'allow_self_signed' => true
+          )
+      );
 
     while( $row3 = mysqli_fetch_array($result3)) { 
     if ( ($row3['reservePrice']) <= ($row3['bidPrice']) ){
@@ -64,11 +102,32 @@ if ($conn->query($sql) === TRUE) {
     } else {
         echo "Error: " . $sql4 . "<br>" . $conn->error;
     }
-
-
-
+    }
+    else {
+      $productName = $row3["productName"];
+      $mail->ClearAllRecipients();
+      $mail->Subject = 'UCL Databases';
+      $mail->Debugoutput = 'html';
+      $mail->setFrom('ebaeauction@gmail.com', 'eBae Auction');
+      $mail->addAddress($row3['email_ID'], 'Sellers');
+      $mail->Subject = 'Auction Successful';
+      $mail->Debugoutput = 'html';
+      $mail->Body = 'Hi, 
+                    Sorry, you have not sold product: '.$productName.' 
+                    Come back soon!';
+    
+      if ($mail->send()){
+          echo 'Message sent';
+      }
+    
+        echo json_encode($mail);
+      
+      }    
 
     }
+
+
+
 }
   
   
