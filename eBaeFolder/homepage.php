b823fba9f361ca109af2c019bd6b3849853c33db
Sameen Asghar <Sameen@pc-212-233.customer.sfields.nido.lan>
2018-03-14 13:35:04 +0000

    20

--- eBaeFolder/homepage.php
+++ eBaeFolder/homepage.php
@@ -141,8 +141,8 @@ $(document).ready(function(){
 
   <!-- Live Auctions -->
 
-   <input id="myInput" type="text" placeholder="Search for items 40.." class="w3-third w3-container" style = "position: relative;
-    left: 40px;">
+   <input id="myInput" type="text" placeholder="Search for items.." class="w3-third w3-container" style = "position: relative;
+    left:20px;">
   <br><br>
   <div class="w3-container" id="myDIV">
   <?php
