a253661e6278364ffeedd96e677281899102a803
Amelia <ameliaoduffy@Amelias-MacBook-Pro.local>
2018-03-14 10:03:00 +0000

    pls work

--- eBaeFolder/deletephp.php
+++ eBaeFolder/deletephp.php
@@ -14,12 +14,16 @@ if (isset($_POST['submit']))
   
 
  
-  $sql = "DELETE product FROM product INNER JOIN auction ON product.productID = auction.productID WHERE product.productID = auction.productID = 61";
+  $sql1 = "DELETE from auction WHERE productID = $productID";
+  $sql2 = "DELETE from viewingtraffic WHERE productID = $productID";
+  $sql3 = "DELETE from bid WHERE productID = $productID";
+  $sql4 = "DELETE from feedback WHERE productID = $productID";
+  $sql5 = "DELETE from product WHERE productID = $productID";
        
       
   mysqli_query($sql, $conn);
 
-  if (($conn->query($sql) === TRUE)) {
+  if (($conn->query($sql1) === TRUE) && ($conn->query($sql2) === TRUE) && ($conn->query($sql3) === TRUE) && ($conn->query($sql4) === TRUE) && ($conn->query($sql5) === TRUE)) {
     echo "Product deleted successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
