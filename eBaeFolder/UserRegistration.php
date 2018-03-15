a7fc2f08251d5f694ff9f0313da884a0144c17d9
jMCsetton <jMCsetton@github.com>
2018-03-15 15:42:01 +0000

    little format change

--- eBaeFolder/UserRegistration.php
+++ eBaeFolder/UserRegistration.php
@@ -165,11 +165,11 @@ function validationform() {
     return false;
   }
   if (isValidDate(dbirth) == false) {
-    alert("Please enter a valid date of birth! (mm/dd/yyyy)");
+    alert("Please enter a valid date of birth! (yyyy/mm/dd)");
     return false;
   }
   if (inpast(dbirth) == false) {
-    alert("Hello time traveller! Tell us how you did it or you're not allowed to use our site. Or please enter a valid date of birth! (mm/dd/yyyy)");
+    alert("Hello time traveller! Tell us how you did it or you're not allowed to use our site. Or please enter a valid date of birth! (yyyy/mm/dd)");
     return false;
   }
 
