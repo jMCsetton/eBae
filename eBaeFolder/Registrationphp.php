e2d31c623b589793dc6dea202601019815f9cbca
Shabri <home.shabri@gmail.com>
2018-03-15 13:55:51 +0000

    password enc

--- eBaeFolder/Registrationphp.php
+++ eBaeFolder/Registrationphp.php
@@ -13,6 +13,7 @@ if (isset($_POST['submit']))
   }
 
   $pword = $_POST["password"];
+  $enc_pword = md5($pword);
   
 
 
@@ -20,7 +21,7 @@ if (isset($_POST['submit']))
   mysqli_query($sql, $conn);
   $sql2 = "INSERT INTO user (username, firstName, lastName, DOB, gender, email_ID, postCode, role, password)
   VALUES ('".$_POST["username"]."', '".$_POST["firstName"]."', '".$_POST["lastName"]."', '".$_POST["DOB"]."',
-  '".$_POST["gender"]."', '".$_POST["email_ID"]."', '".$_POST["postCode"]."', 'buyer_seller','md5($pword))";
+  '".$_POST["gender"]."', '".$_POST["email_ID"]."', '".$_POST["postCode"]."', 'buyer_seller','$enc_pword')";
   mysqli_query($sql2, $conn);
 
   if (($conn->query($sql) === TRUE) && ($conn->query($sql2) === TRUE)) {
